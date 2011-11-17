778817dccd1f68dd8d0ff24e882b6714ce9a1ac1
danx0r <danbmil99@gmail.com>
2011-11-16 18:33:16 -0800

    formatting

--- keep.py
+++ keep.py
@@ -30,7 +30,39 @@ for i in range(len(lines)):
         mn = int(tim[1])
         sec = int(tim[2])
         comment = lines[i+4].strip()
+        print commit, yr, months[mo], date, hr, mn, sec, comment
+
+        #convert into day + hour since 2010
         day = datetime.date(yr, mo+1, date+1).toordinal() - day0
         hour = hr + mn/60.0 + sec/3600.0
-        print commit, day, hour, yr, months[mo], date, hr, mn, sec, comment
-        
+
+        #move it all back 6 hours (work cycle from 6am - 6am)
+        hour -= 6
+        if hour < 0:
+            hour += 24
+            day -= 1
+
+        if day not in days:
+            days[day] = {
+                'hist':[0 for i in range(24)],
+                'commits':[],
+                'comments':[]
+                }
+        days[day]['commits'].append(commit)
+        days[day]['comments'].append(comment)
+        hist = days[day]['hist']
+        ihr = int(hour)
+        hist[ihr] += 1
+
+print "----------------------------------------------------------------------"
+keys = days.keys()
+keys.sort()
+for i in keys:
+    day = days[i]
+    h = ""
+    for j in day['hist']:
+        if j:
+            h += "%3d" % j
+        else:
+            h += "   "
+    print h, day['commits'][-1][:6], day['comments']
